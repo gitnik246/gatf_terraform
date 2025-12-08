c4aa61dde86dc9fe6f8cf9038d02f49a0bbfacb4
Nicolas Garcia <nicolas246@gmail.com>
2025-12-08 17:09:49 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ data "aws_ami" "amazon_linux2" {
 
 resource "aws_instance" "example" {
   ami           = data.aws_ami.amazon_linux2.id
-  instance_type = "t2.micro"   # free-tier eligible
+  instance_type = "t3.micro"   # free-tier eligible
 
   tags = {
     Name = "tf-study-instance"
