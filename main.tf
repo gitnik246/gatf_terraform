fc7068ad255db9717a58f124bdd39a96ed038f62
Nicolas Garcia <nicolas246@gmail.com>
2025-12-08 16:48:25 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-east-1"
+  # Ohio (the region shown in your screenshot)
+  region = "us-east-2"
+}
+
+# Get the latest Amazon Linux 2 AMI in this region (no hard-coded ID)
+data "aws_ami" "amazon_linux2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-}
+  ami           = data.aws_ami.amazon_linux2.id
+  instance_type = "t2.micro"   # free-tier eligible
 
-# test deploy
+  tags = {
+    Name = "tf-study-instance"
+  }
+}
